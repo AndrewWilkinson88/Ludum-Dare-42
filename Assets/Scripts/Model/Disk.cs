54138711921b8403f0f2a35447b37376e814763b
Kevin Kohler <kkohler@hitpointstudios.com>
2018-08-11 14:37:52 -0400

    Started adding art

--- Assets/Scripts/Model/Disk.cs
+++ Assets/Scripts/Model/Disk.cs
@@ -8,10 +8,17 @@ namespace deleteAfterReading.Model
     [Serializable]
     public class Disk
     {
+        //Time the floppy is dropped onto the screen (maybe not applicable?)
         public int start;
+        //Title on the floppy
+        public string title;
+        //Who the message is to
         public string to;
+        //Who the message is from
         public string from;
+        //body of the message
         public string text;
+        //keywords in the message
         public List<string> keywords;
     }
 }
