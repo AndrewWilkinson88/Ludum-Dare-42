1f844950213412a10173f4acb609057767bdd85b
awilkinson <AndrewWilkinson88@gmail.com>
2018-08-12 19:13:21 -0400

    Added intro animation

--- Assets/Scripts/Computer Controllers/MainMenuView.cs
+++ Assets/Scripts/Computer Controllers/MainMenuView.cs
@@ -7,6 +7,8 @@ public class MainMenuView : MonoBehaviour
 {
     public SpriteRenderer computerBackground;
     public SpriteRenderer titleScreen;
+    public SpriteRenderer stickyNote;
+    public SpriteRenderer stickyArm;
     public Color OnColor;
 
     public TextButton mission1;
@@ -18,6 +20,9 @@ public class MainMenuView : MonoBehaviour
  
         startSequence.Append(computerBackground.DOColor(OnColor, 1.0f).SetEase(Ease.OutFlash, 15, 1));
         startSequence.Append(titleScreen.transform.DOLocalMoveY(3.0f, 1.0f));
+        startSequence.Append(stickyNote.transform.DOLocalMoveY(2.5f, 0.5f).SetEase(Ease.InExpo));
+        startSequence.Join(stickyArm.transform.DOLocalMoveY(-4, 0.5f).SetEase(Ease.InExpo));
+        startSequence.Append(stickyArm.transform.DOLocalMoveY(-13, 0.5f).SetEase(Ease.InExpo));
         startSequence.AppendCallback(() => mission1.gameObject.SetActive(true));
         startSequence.Play();
     }
