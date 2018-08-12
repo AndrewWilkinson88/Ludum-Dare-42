bfaa0466eecb849733d6487d6d4b75c6149d76bf
Kevin Kohler <kkohler@hitpointstudios.com>
2018-08-12 14:19:52 -0400

    More ResultScreen work.  Now updates image and text based on result.

--- Assets/Scripts/ResultScreenController.cs
+++ Assets/Scripts/ResultScreenController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 
@@ -9,8 +10,14 @@ namespace DeleteAfterReading
 
     public class ResultScreenController : MonoBehaviour
     {
-        
+        public Image successImage;
+        public Image failImage;
+
         public TextMeshProUGUI headlineText;
+        public TextMeshProUGUI bodyText;
+
+        private string successBody = "With the aid of a new specially trained agent, the latest CIA mission was a resounding success!  America is safe once more.  When asked how he accomplished the a task with such limited resources the agent only responded \"I dug through a lot of";
+        private string failBody = "It is with great dismay that we report to you on a failed operation.  A new agent has brough shame not only to the CIA, but to the entire country!  Our security has never been more at risk then it is today.  When asked for comment the agent only said ";
 
         // Use this for initialization
         void Start()
@@ -26,13 +33,21 @@ namespace DeleteAfterReading
         public void ShowNewspaperSuccess(string headline)
         {
             gameObject.SetActive(true);
+            successImage.gameObject.SetActive(true);
+            failImage.gameObject.SetActive(false);
+
             headlineText.text = headline;
+            bodyText.text = successBody;
         }
 
         public void ShowNewspaperFailure(string failedBlob)
         {
             gameObject.SetActive(true);
+            successImage.gameObject.SetActive(false);
+            failImage.gameObject.SetActive(true);
+
             headlineText.text = failedBlob + "?  Agent bungles mission.";
+            bodyText.text = failBody;
         }
 
         public void HandleNext()
